655e87c5f1452cd5bdde0ca566c13fc70bedd002
Romain Dallet <romain.dallet@sb-roscoff.fr>
2018-11-26 17:01:41 +0100

    Add k row/col option

--- app.R
+++ app.R
@@ -13,16 +13,6 @@ data_table <- t(as.matrix(read.table("inputdata.tsv", header=TRUE, check.names=F
 # User interface ----
 ui <- fluidPage(
 	includeCSS("styles.css"),
-	fluidRow(
-		h5(strong("Wich package :")),
-		switchInput(
-			inputId = "choice",
-			label = strong("Package"),
-			value = FALSE,
-			offLabel = "Heatmaply",
-			onLabel = "D3heatmap"
-        )
-	),
     fluidRow(
 		conditionalPanel(
 			condition = "input.choice == false",
@@ -32,7 +22,39 @@ ui <- fluidPage(
 			condition = "input.choice == true", 
 			d3heatmapOutput('HEATMAP_d3')
 		)
-    )
+    ),
+	fluidRow(
+		column(4,
+			h5(strong("Wich package :")),
+			switchInput(
+				inputId = "choice",
+				label = strong("Package"),
+				value = FALSE,
+				offLabel = "Heatmaply",
+				onLabel = "D3heatmap"
+	        )
+		),
+		column(4,
+			numericInput(
+				inputId = "krow",
+				label = "k row :",
+				value = 3,
+				min = 1,
+				max = 10,
+				step = 1
+			)
+		),
+		column(4,
+			numericInput(
+				inputId = "kcol",
+				label = "k col :",
+				value = 3,
+				min = 1,
+				max = 10,
+				step = 1
+			)
+		)
+	)
 )
 
 # Server logic ----
@@ -40,12 +62,12 @@ server <- function(input, output) {
 
 	# dendrogram = "both", k_row = nb_row
     output$HEATMAP_plotly <- renderPlotly({
-        graph_plotly <- heatmaply(data_table, scale = "column", dendrogram = "both", k_row = 3, colors = Spectral(20)) 
+        graph_plotly <- heatmaply(data_table, scale = "column", dendrogram = "both", k_row = input$krow, k_col = input$kcol, colors = Spectral(20)) 
         return(graph_plotly)
     })
 
     output$HEATMAP_d3 <- renderD3heatmap({
-		graph_d3 <- d3heatmap(data_table, scale = "column", dendrogram = "row", k_row = 3, colors = Spectral(20))
+		graph_d3 <- d3heatmap(data_table, scale = "column", dendrogram = "both", k_row = input$krow, k_col = input$kcol, colors = Spectral(20))
         return(graph_d3)
     })
 }
